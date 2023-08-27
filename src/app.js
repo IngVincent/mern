9f48ab6cf77c86245ffff037ef376298adbad849
Vicente <lazvinz21@gmail.com>
2023-08-27 16:00:07 -0600

    fixed auth.routes.js name

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser';
 
 //app es el servidor
